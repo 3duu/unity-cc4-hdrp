b4cd58469071f3581ed5e55ff21450e2d769f269
3duu <eduu.porto@gmail.com>
2025-12-10 10:09:26 -0300

    Fix formatting in AnimPlayerGUI; remove unnecessary attribute

--- Editor/AnimPlayerGUI.cs
+++ Editor/AnimPlayerGUI.cs
@@ -204,7 +204,7 @@ namespace Reallusion.Import
         private static string paramDirection = "param_direction";
         [SerializeField]
         private static AnimatorState playingState;
-        [SerializeField]
+     
         public static int controlStateHash { get; set; }
 
         // animator/animation settings
